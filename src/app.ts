ff26f2c5a8efc215aace34083232d10c3810c454
FoxBeCoding <fox@foxbecoding.com>
2023-06-29 12:10:06 -0500

    updated app

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,6 @@ dotenv.config()
 const app: Express = express();
 const port: string|number = process.env.PORT || 6000
 
-app.get('/assets/:file', (req: Request, res: Response) => {
-    // Handle the static asset request here
-})
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server')
-});
   
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
