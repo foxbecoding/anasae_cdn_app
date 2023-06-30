449180e069cb69193790479e877bb33c268b38a8
FoxBeCoding <fox@foxbecoding.com>
2023-06-30 12:08:18 -0500

    new dist build

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.routes = void 0;
 const express_1 = __importDefault(require("express"));
+const mediaRoutes_1 = require("./mediaRoutes");
 const defaultRoute_1 = require("./defaultRoute");
 exports.routes = express_1.default.Router();
 exports.routes.use(defaultRoute_1.defaultRoute);
+exports.routes.use(mediaRoutes_1.mediaRoute);
--- dist/routes/mediaRoutes.js
+++ dist/routes/mediaRoutes.js
@@ -5,11 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mediaRoute = void 0;
 const express_1 = __importDefault(require("express"));
+const mediaControllers_1 = require("../Controllers/mediaControllers");
 exports.mediaRoute = express_1.default.Router();
-exports.mediaRoute.get('/media/:file', (req, res) => {
-    // console.log(req.body)
-    // res.send()
-});
+exports.mediaRoute.post('/add-image/', mediaControllers_1.AddImageController);
 // app.get('/', (req: Request, res: Response) => {
 //     res.send('Express + TypeScript Server')
 // });
