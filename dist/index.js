fc1b206e640ecf125d65c871440972ae6d7b6ed4
FoxBeCoding <fox@foxbecoding.com>
2023-07-23 19:52:15 -0500

    updated src, dist

--- dist/index.js
+++ dist/index.js
@@ -6,14 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const routes_1 = require("./routes");
+const http_1 = __importDefault(require("http"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const env = process.env;
 const port = process.env.PORT || 3058;
+const host = process.env.HOST || '127.0.0.1';
 const media_root = env.NODE_ENV === 'development' ? env.MEDIA_ROOT_DEV : env.MEDIA_ROOT_PRO;
+const server = http_1.default.createServer(app);
 // routes
 app.use('/', routes_1.routes);
 app.use(express_1.default.static(`${media_root}`));
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://127.0.0.1:${port}`);
+// app.listen(port, () => {
+//     console.log(`[server]: Server is running at http://127.0.0.1:${port}`)
+// })
+server.listen(port, host);
+server.on('listening', function () {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import express, { Express } from 'express'
 import dotenv from 'dotenv'
 import { routes } from './routes'
+import http from 'http'
 
 dotenv.config()
 const app: Express = express()
 const env: NodeJS.ProcessEnv = process.env
 const port: string|number = process.env.PORT || 3058
+const host: string = process.env.HOST || '127.0.0.1'
 const media_root = env.NODE_ENV === 'development' ? env.MEDIA_ROOT_DEV : env.MEDIA_ROOT_PRO
+const server: any = http.createServer(app);
 
 // routes
 app.use('/', routes);
 app.use(express.static(`${media_root}`))
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://127.0.0.1:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`[server]: Server is running at http://127.0.0.1:${port}`)
+// })
+
+server.listen(port, host);
+server.on('listening', function() {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+});
+
