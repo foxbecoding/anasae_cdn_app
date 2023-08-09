7c2657dfd747f94d5a6325a02e3b123e81b9afb7
FoxBeCoding <fox@foxbecoding.com>
2023-08-09 10:33:14 -0500

    updated cors

--- dist/middleware/cors.js
+++ dist/middleware/cors.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptionsDelegate = void 0;
-const allowlist = ['127.0.0.1:3058', '127.0.0.1:8000', 'localhost'];
+const allowlist = [
+    '127.0.0.1:3058',
+    '127.0.0.1:8000',
+    '192.168.1.235:3058',
+    'anasae.local'
+];
 const corsOptionsDelegate = (req, callback) => {
     req.headers.origin = req.headers.origin || req.headers.host;
     let corsOptions = allowlist.indexOf(req.headers.origin) !== -1 ? { origin: true } : { origin: false };
--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,8 @@
 const allowlist = [
     '127.0.0.1:3058', 
     '127.0.0.1:8000', 
-    '192.168.1.235:3058'
+    '192.168.1.235:3058',
+    'anasae.local'
 ]
 
 export const corsOptionsDelegate = (req: any, callback: Function) => {
