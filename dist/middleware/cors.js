1679aa88ba93f1b85e5f5580be984e5c892de7c1
FoxBeCoding <fox@foxbecoding.com>
2023-10-18 19:05:34 -0500

    updated cors

--- dist/middleware/cors.js
+++ dist/middleware/cors.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptionsDelegate = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const env = process.env;
 const allowlist = [
     `${env.ALLOWED_HOST1}`,
